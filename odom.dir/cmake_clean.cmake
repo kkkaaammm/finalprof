7eb595bf5ce23ec2c48affff96e0acc1eab79e55
kkkaaammm <92441378+kkkaaammm@users.noreply.github.com>
2021-10-13 19:55:32 +0700

    Add files via upload

---
+++ odom.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/kg/Test/devel/lib/odometry_setup/odom"
+  "/home/kg/Test/devel/lib/odometry_setup/odom.pdb"
+  "CMakeFiles/odom.dir/src/odom.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/odom.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
