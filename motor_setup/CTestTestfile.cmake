d33d6f679333cb7e6d22e8b56135b061b087759b
kkkaaammm <92441378+kkkaaammm@users.noreply.github.com>
2021-10-13 19:32:41 +0700

    Add files via upload

---
+++ motor_setup/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kg/Test/src/motor_setup
+# Build directory: /home/kg/Test/build/motor_setup
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
